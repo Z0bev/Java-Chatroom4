0d784945b57f4c38f1cd93b8c04e851c500b48e4
samuelzobev <szobev@gmail.com>
2023-03-19 10:40:13 +0000

    comments included

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -7,20 +7,20 @@ import java.util.*;
 
 public class ChatClient {
 
-    private JFrame frame;
-    private JTextArea chatArea;
-    private JTextField messageField;
-    private JList<String> clientList;
-    private DefaultListModel<String> clientListModel;
-    private PrintWriter output;
-    private String username;
+    private JFrame frame; // declare a JFrame instance variable called frame
+    private JTextArea chatArea; // declare a JTextArea instance variable called chatArea
+    private JTextField messageField; // declare a JTextField instance variable called messageField
+    private JList<String> clientList; // declare a JList instance variable called clientList
+    private DefaultListModel<String> clientListModel; // declare a DefaultListModel instance variable called clientListModel
+    private PrintWriter output; // declare a PrintWriter instance variable called output
+    private String username; // declare a String instance variable called username
 
     public ChatClient() {
         // Prompt the user for a username
         username = JOptionPane.showInputDialog(frame, "Enter username:", "Username", JOptionPane.PLAIN_MESSAGE);
 
         // Create the GUI components
-        frame = new JFrame(username);
+        frame = new JFrame(username); // create a new JFrame instance called frame and set the title to the username
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel(new BorderLayout());
@@ -32,35 +32,35 @@ public class ChatClient {
         messageField = new JTextField();
         messagePanel.add(messageField, BorderLayout.CENTER);
         JButton sendButton = new JButton("Send");
-        sendButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sendMessage(messageField.getText());
-                messageField.setText("");
-            }
+        // add an ActionListener to the sendButton
+        sendButton.addActionListener(e -> {
+            sendMessage(messageField.getText()); // call the sendMessage method with the text from the JTextField as an argument
+            messageField.setText(""); // clear the text from the JTextField
         });
         messagePanel.add(sendButton, BorderLayout.EAST);
         panel.add(messagePanel, BorderLayout.SOUTH);
-        clientListModel = new DefaultListModel<String>();
-        clientList = new JList<String>(clientListModel);
-        JScrollPane clientScrollPane = new JScrollPane(clientList);
+        clientListModel = new DefaultListModel<String>(); // create a new DefaultListModel instance called clientListModel
+        clientList = new JList<String>(clientListModel); // create a new JList instance called clientList and pass the DefaultListModel as an argument
+        JScrollPane clientScrollPane = new JScrollPane(clientList); // create a new JScrollPane instance called clientScrollPane and add the JList to it
         clientScrollPane.setPreferredSize(new Dimension(150, 0));
         panel.add(clientScrollPane, BorderLayout.EAST);
-        frame.add(panel);
-        frame.setVisible(true);
+        frame.add(panel); // add the panel to the JFrame
+        frame.setVisible(true); // set the JFrame to be visible
 
         // Connect to the server and start listening for messages
         try {
-            Socket socket = new Socket("localhost", 9000);
+            Socket socket = new Socket("localhost", 9000); // Connect to the server on localhost:9000
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             output = new PrintWriter(socket.getOutputStream(), true);
-            output.println(username);
+            output.println(username); // Send the username to the server
             new Thread(() -> {
                 try {
                     while (true) {
-                        String message = input.readLine();
+                        String message = input.readLine(); // Read messages from the server
                         if (message == null) {
                             break;
                         }
+                        updateChatArea(message); // Update the chat area with the received message
                     }
                 } catch (IOException e) {
                     System.out.println("Error receiving message from server: " + e.getMessage());
@@ -72,7 +72,7 @@ public class ChatClient {
                     }
                     System.exit(0);
                 }
-            }).start();
+            }).start(); // Start a new thread to handle incoming messages
         } catch (IOException e) {
             System.out.println("Error connecting to server: " + e.getMessage());
             JOptionPane.showMessageDialog(frame, "Error connecting to server. Exiting...", "Error", JOptionPane.ERROR_MESSAGE);
@@ -80,20 +80,22 @@ public class ChatClient {
         }
     }
 
+
     private void updateChatArea(String message) {
         SwingUtilities.invokeLater(() -> {
             chatArea.append(message + "\n");
-            chatArea.setCaretPosition(chatArea.getDocument().getLength());
+            chatArea.setCaretPosition(chatArea.getDocument().getLength()); // Scroll to the bottom of the chat area
         });
     }
 
+    // Method to send a message to the server
     private void sendMessage(String message) {
         if (!message.isEmpty()) {
             output.println(message);
         }
     }
 
-
+    // Main method to start the chat client
     public static void main(String[] args) {
         new ChatClient();
     }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -6,12 +6,14 @@ import javax.swing.*;
 
 public class ChatServer extends JFrame implements ActionListener {
 
+    // GUI elements
     private JTextArea chatArea;
     private JTextField chatInput;
     private JButton sendButton;
     private JList<String> clientList;
     private DefaultListModel<String> clientListModel;
 
+    // Server elements
     private ServerSocket serverSocket;
     private Thread serverThread;
     private boolean running;
@@ -21,47 +23,47 @@ public class ChatServer extends JFrame implements ActionListener {
 
         // Set up GUI
         JPanel chatPanel = new JPanel(new BorderLayout());
-        chatArea = new JTextArea(10, 30);
-        chatArea.setEditable(false);
-        chatArea.setLineWrap(true);
-        chatPanel.add(new JScrollPane(chatArea), BorderLayout.CENTER);
+        chatArea = new JTextArea(10, 30); // create a text area for displaying messages
+        chatArea.setEditable(false); // disable editing of the text area
+        chatArea.setLineWrap(true); // enable line wrapping in the text area
+        chatPanel.add(new JScrollPane(chatArea), BorderLayout.CENTER); // add the text area to a scroll pane and add the pane to the chat panel
         JPanel inputPanel = new JPanel(new BorderLayout());
         JPanel clientPanel = new JPanel(new BorderLayout());
-        JLabel clientLabel = new JLabel("Connected Clients");
-        clientPanel.add(clientLabel, BorderLayout.NORTH);
+        JLabel clientLabel = new JLabel("Connected Clients"); // create a label for the client list
+        clientPanel.add(clientLabel, BorderLayout.NORTH); // add the label to the client panel
         clientListModel = new DefaultListModel<String>();
-        clientList = new JList<String>(clientListModel);
-        clientPanel.add(new JScrollPane(clientList), BorderLayout.CENTER);
-        chatPanel.add(clientPanel, BorderLayout.EAST);
-        getContentPane().add(chatPanel);
+        clientList = new JList<String>(clientListModel); // create a list for displaying connected clients
+        clientPanel.add(new JScrollPane(clientList), BorderLayout.CENTER); // add the client list to a scroll pane and add the pane to the client panel
+        chatPanel.add(clientPanel, BorderLayout.EAST); // add the client panel to the right side of the chat panel
+        getContentPane().add(chatPanel); // add the chat panel to the content pane of the JFrame
 
         // Set up server
         running = false;
         serverSocket = null;
 
-        setSize(600, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        setSize(600, 300); // set the size of the JFrame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set the close operation of the JFrame
+        setVisible(true); // make the JFrame visible
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == sendButton) {
-            String message = chatInput.getText().trim();
-            if (!message.equals("")) {
-                broadcastMessage("Server", message);
-                chatInput.setText("");
+        if (e.getSource() == sendButton) { // check if the send button was clicked
+            String message = chatInput.getText().trim(); // get the text from the input field and remove leading and trailing whitespace
+            if (!message.equals("")) { // check if the message is not empty
+                broadcastMessage("Server", message); // broadcast the message to all connected clients
+                chatInput.setText(""); // clear the input field
             }
         }
     }
 
     private void startServer() {
         try {
-            serverSocket = new ServerSocket(9000);
+            serverSocket = new ServerSocket(9000); // create a server socket
             running = true;
-            while (running) {
-                Socket clientSocket = serverSocket.accept();
-                ClientThread clientThread = new ClientThread(clientSocket);
-                clientThread.start();
+            while (running) { // continuously accept new client connections
+                Socket clientSocket = serverSocket.accept(); // accept a new client connection
+                ClientThread clientThread = new ClientThread(clientSocket); // create a new thread for the client
+                clientThread.start(); // start the thread
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -69,23 +71,21 @@ public class ChatServer extends JFrame implements ActionListener {
     }
 
     private synchronized void addClient(String clientName) {
-        clientListModel.addElement(clientName);
+        clientListModel.addElement(clientName); // add a client to the client list
     }
 
     private synchronized void removeClient(String clientName) {
-        clientListModel.removeElement(clientName);
+        clientListModel.removeElement(clientName); // remove a client from the client list
     }
 
     private synchronized void broadcastMessage(String sender, String message) {
-        chatArea.append(sender + ": " + message + "\n");
-        chatArea.setCaretPosition(chatArea.getDocument().getLength());
-        for (int i = 0; i < clientListModel.size(); i++) {
-            String clientName = clientListModel.getElementAt(i);
-            if (!clientName.equals("Server")) {
-                ClientThread clientThread = (ClientThread) Thread.currentThread();
-                if (!clientThread.clientName.equals(clientName)) {
-                    clientThread.sendMessage(sender, message);
-                }
+        chatArea.append(sender + ": " + message + "\n"); // add the message to the chat area
+        chatArea.setCaretPosition(chatArea.getDocument().getLength()); // set the chat area caret to the end of the text
+        for (int i = 0; i < clientListModel.size(); i++) { // loop through all the clients
+            String clientName = clientListModel.getElementAt(i); // get the name of the current client
+            ClientThread clientThread = (ClientThread) Thread.currentThread(); // get the current client thread
+            if (!clientThread.clientName.equals(clientName)) { // make sure the message is not sent to the client who sent it
+                    clientThread.sendMessage(sender, message); // send the message
             }
         }
     }
@@ -110,23 +110,23 @@ public class ChatServer extends JFrame implements ActionListener {
 
                 String line;
                 while ((line = input.readLine()) != null) {
-                    lastActiveTime = System.currentTimeMillis();
+                    lastActiveTime = System.currentTimeMillis(); // update the last active time of the client
 
-                    if (clientName.equals("")) {
-                        clientName = line.trim();
-                        addClient(clientName);
-                        broadcastMessage("Server", clientName + " has joined the chatroom.");
+                    if (clientName.equals("")) { // if the client name is not set yet
+                        clientName = line.trim(); // set the client name to the received input
+                        addClient(clientName); // add the client to the client list
+                        broadcastMessage("Server", clientName + " has joined the chatroom."); // broadcast a message to all clients
                     } else {
-                        broadcastMessage(clientName, line);
+                        broadcastMessage(clientName, line); // broadcast the client's message to all clients
                     }
                 }
 
-                removeClient(clientName);
-                broadcastMessage("Server", clientName + " has left the chatroom.");
-                clientSocket.close();
+                removeClient(clientName); // remove the client from the client list
+                broadcastMessage("Server", clientName + " has left the chatroom."); // broadcast a message to all clients
+                clientSocket.close(); // close the client socket
             } catch (IOException e) {
-                removeClient(clientName);
-                broadcastMessage("Server", clientName + " has left the chatroom.");
+                removeClient(clientName); // remove the client from the client list
+                broadcastMessage("Server", clientName + " has left the chatroom."); // broadcast a message to all clients
             }
         }
 
@@ -139,6 +139,6 @@ public class ChatServer extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         ChatServer server = new ChatServer();
-        server.startServer();
+        server.startServer(); // start the server
     }
 }
